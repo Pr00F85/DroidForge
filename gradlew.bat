660d4fd63ae8c23ec0e460593419670e88bc5ecd
Pr00F85 <jamesbragayrac@proton.me>
2026-03-11 17:27:43 -0400

    [DroidForge] Update gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,12 @@
+@rem Gradle wrapper script for Windows
+@if "%DEBUG%"=="" @echo off
+if "%OS%"=="Windows_NT" setlocal
+set APP_HOME=%~dp0
+set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+set JAVA_EXE=java.exe
+%JAVA_EXE% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+if "%ERRORLEVEL%"=="0" goto mainEnd
+:fail
+exit /b 1
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
